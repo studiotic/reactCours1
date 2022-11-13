a82dfd60d92ab9e47b645dfce0b90aa4d70ce95b
studiotic <agence.studiotc@gmail.com>
2022-11-13 18:43:50 +0100

    premier commit

---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Countries from '../components/Countries';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <Countries />
+            <h1>ici accueil </h1>
+            <br />
+            <p>Lorem ipsum Lorem ipsum Lorem ipsum
+                Lorem ipsum Lorem ipsum Lorem ipsum
+                Lorem ipsum Lorem ipsum Lorem ipsum
+                Lorem ipsum Lorem ipsum Lorem ipsum
+                Lorem ipsum Lorem ipsum Lorem ipsum
+                Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+                ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                Lorem ipsum Lorem ipsum Lorem ipsum Lorem i
+                psum Lorem ipsum Lorem ipsum Lorem ipsum L
+                orem ipsum Lorem ipsum Lorem ipsum Lor
+                em ipsum Lorem ipsum Lorem ipsum Lor
+                em ipsum Lorem ipsum Lorem ipsum
+            </p>
+
+
+        </div>
+    );
+};
+
+export default Home;
